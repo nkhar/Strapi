35784d2dd8cf7c41515f5858137ac3e7cd0b3041
nkhar iMac <nikoloz.kharshiladze.2014@live.rhul.ac.uk>
2022-11-18 12:20:46 +0400

    strapi runs on local. Added service content.

--- config/admin.js
+++ config/admin.js
@@ -1,9 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'e4350524b0689fc3f46e2407dae950e2'),
+    secret: env("ADMIN_JWT_SECRET", "e4350524b0689fc3f46e2407dae950e2"),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("API_TOKEN_SALT", "Y9zJWruUclFdNBBkaQihBJ6t4cKBskAbitf1c6Hsa6s="),
   },
-
 });
