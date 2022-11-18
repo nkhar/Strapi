9308248c688fc2f427be34d49ead4a4a1b816321
nkhar iMac <nikoloz.kharshiladze.2014@live.rhul.ac.uk>
2022-11-18 23:29:32 +0400

    Created plugins.js to use Cloudinary only in prod.

--- config/plugins.js
+++ config/plugins.js
@@ -1,21 +1,16 @@
 module.exports = ({ env }) => ({
-    'users-permissions': {
-      config: {
-      jwtSecret: env('JWT_SECRET'),
-      },
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET"),
     },
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
+  },
+  upload: {
+    config: {
+      providerOptions: {
+        localServer: {
+          maxage: 300000,
         },
       },
     },
-  });
+  },
+});
