b69f912be271ad9b13563444444e2e0c4538d88b
nkhar iMac <nikoloz.kharshiladze.2014@live.rhul.ac.uk>
2023-02-01 04:02:56 +0400

    changed database stuff for AWS RDS POSTTGRES

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,12 +1,13 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'strapi'),
-      user: env('PGUSER', 'strapi'),
-      password: env('PGPASSWORD', 'password'),
+      host: env("PGHOST", "127.0.0.1"),
+      // port: env.int('PGPORT', 5432),
+      port: env.int("PGPORT", 5431),
+      database: env("PGDATABASE", "strapi"),
+      user: env("PGUSER", "postgres"),
+      password: env("PGPASSWORD", "postgres123"),
       ssl: env.bool(true),
     },
   },
